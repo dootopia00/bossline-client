62ec9fa0398fed108135a914c342882908d34849
dooopia <dootopia00@gmail.com>
2022-03-21 01:10:20 +0900

    수정

--- bossline_client/assets/js/common/common.js
+++ bossline_client/assets/js/common/common.js
@@ -20,8 +20,8 @@ if(API_SERVER_HOSTNAME == 'localhost'){
 
 }else{
 
-    API_SERVER = "http://client.bossline.gg";
-    SERVICE_DOMAIN = "http://api.bossline.gg";
+    API_SERVER = "http://api.bossline.gg";
+    SERVICE_DOMAIN = "http://client.bossline.gg";
 }
 
 
